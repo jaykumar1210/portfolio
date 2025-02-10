1e0a6e66c0c947092a4e4874f6f2808736f1d358
jay Kumar <1210jaypatel@gmail.com>
2025-02-10 13:50:30 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar color change on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
